657fd82815a5e231ee262ef018224d10fc80ad04
Joseph Fitzpatrick <joeyf116@yahoo.com>
2019-12-09 15:56:10 -0500

    addMember POST route added

--- backend/models/memberModel.js
+++ backend/models/memberModel.js
@@ -1,17 +1,16 @@
 const db = require('./conn');
 
 class Member {
-    constructor(id, name, email, dob, datejoined) {
+    constructor(id, name, email, phone, age, status, waiver, contract, date_joined) {
       this.id = id;
       this.name = name;
       this.email = email;
       this.phone = phone;
       this.age = age;
-      this.phone = phone;
       this.status = status;
       this.waiver = waiver;
       this.contract = contract;
-      this.datejoined = datejoined;
+      this.date_joined = date_joined;
     }
   
     static async getMember() {
@@ -22,6 +21,19 @@ class Member {
         return err.message;
       }
     }
+
+    static async addMember(name, email, phone, age, status, waiver, contract, date_joined) {
+        const query = `INSERT INTO members (name, email, phone, age, status, waiver, contract, date_joined) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
+    
+        try {
+          const response = await db.one(query, [name, email, phone, age, status, waiver, contract, date_joined]);
+          return response;
+        } catch (err) {
+          return err.message;
+        }
+      }
+
+
 }
 
 module.exports = Member;
--- backend/routes/member.js
+++ backend/routes/member.js
@@ -8,4 +8,10 @@ router.get('/', async (req, res, next) => {
     res.json(getMember).status(200);
   })
 
+  router.post("/addMember", async (req, res) => {
+    const { name, email, age, phone, status, waiver, contract, date_joined } = req.body;
+    const response = await memberModel.addMember(name, email, age, phone, status, waiver, contract, date_joined);
+      res.sendStatus(200);
+  });
+
 module.exports = router;
