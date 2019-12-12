8128723f424008afdcf21b027a4f0a108005a18b
Joseph Fitzpatrick <joeyf116@yahoo.com>
2019-12-12 12:16:06 -0500

    Member Profile Routes setup to Specific member Id when selected in table

--- backend/models/memberModel.js
+++ backend/models/memberModel.js
@@ -22,8 +22,7 @@ class Member {
         return err.message;
       }
     }
-
-
+    
     static async addMember(name, email, phone, age, status, waiver, contract, date_joined) {
         const query = `INSERT INTO members (name, email, phone, age, status, waiver, contract, date_joined) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
     
--- front-end/src/components/members/MemberProfile.jsx
+++ front-end/src/components/members/MemberProfile.jsx
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
+import loadData from '../../utils/loadData';
+
 
 class MemberProfile extends Component {
-    
+    state = {
+        members: []
+    }
+
+    async componentDidMount() {
+        const data = await loadData(
+            `http://localhost:3333/member`
+            ); 
+            const members = data;
+
+        this.setState({
+            members
+        })
+        }
+
     render() { 
+        const { members } = this.state;
+        console.log(this.props.match.params.memberid)
         return ( 
         <div>
-            <h1>Hello Member</h1>
+            <h1>Member Profile</h1>
+    {members.map(m => m.id == this.props.match.params.memberid  ?
+                <ul key={m.id} value={m.id} name={m.id}>
+                  <li><b>Name:</b> {m.name}</li>
+                  <li><b>Email:</b> {m.email}</li>
+                  <li><b>Phone:</b> {m.phone}</li>
+                  <li><b>Status</b> {m.status}</li>
+                  </ul>
+                  : null)}
         </div> );
     }
 }
