ba07072bea61aa3aff015df35988059afc52eba4
Felipe Dunbar <felipe.a.dunbar@gmail.com>
2019-12-12 15:13:15 -0500

    Added update stock function on backend

--- backend/routes/member.js
+++ backend/routes/member.js
@@ -21,12 +21,12 @@ router.post("/addmember", async (req, res) => {
 });
 
 router.put("/updateMember", async (req,res) => {
-  const { id, name, email, phone, age, status, waiver, contract, date_joined } = req.body;
-  const response = await memberModel.updateMember(id, name, email, phone, age, status, waiver, contract, date_joined);
+  const { id, member_name, email, phone, age, status, waiver, contract, date_joined } = req.body;
+  const response = await memberModel.updateMember(id, member_name, email, phone, age, status, waiver, contract, date_joined);
   if (response.command === "UPDATE" && response.rowCount >= 1) {
     res.sendStatus(200);
   } else {
-    res.send(`Could not update ${name} for id: ${id}`).status(409);
+    res.send(`Could not update ${member_name} for id: ${id}`).status(409);
   }
 });
 
